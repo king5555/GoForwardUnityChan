54a7560f4d37f6c6d2aa59bf3e4a1a8101e6e841
中西　啓介 <rse43171@nifty.com>
2016-10-18 22:02:25 +0900

    課題完了
    Lesson7の課題完了

--- Assets/script/CubeController.cs
+++ Assets/script/CubeController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CubeController : MonoBehaviour {
+public class CubeController : MonoBehaviour
+{
 
     // キューブの移動速度
     private float speed = -0.2f;
@@ -9,13 +10,23 @@ public class CubeController : MonoBehaviour {
     // 消滅位置
     private float deadLine = -10;
 
+    //**************課題用コードＳ*************************************
+    private AudioSource audioSource;
+
+    //**************課題用コードＥ*************************************
+
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+        //**************課題用コードＳ*************************************
+        this.audioSource = this.GetComponent<AudioSource>();
+        //**************課題用コードＥ*************************************
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         // キューブを移動させる
         transform.Translate(this.speed, 0, 0);
@@ -26,4 +37,15 @@ public class CubeController : MonoBehaviour {
             Destroy(gameObject);
         }
     }
+
+    //**************課題用コードＳ*************************************
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "UnityChan") {
+
+            this.audioSource.Play();
+
+        }
+    }
+    //**************課題用コードＥ*************************************
 }
